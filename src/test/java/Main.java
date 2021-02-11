9c1df2f1dfb4c0b4051c286d75b675b829e4ca7d
Sumeyra89 <72941277+Sumeyra89@users.noreply.github.com>
2021-02-11 05:54:43 -0800

    This is first commit

---
+++ src/test/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    //this our group project practice
+    //This our group is the best
+}
